5412dd74aa6a97e7575c1ade0db8f197203170fb
art-ist <dev@art-ist.cc>
2014-05-31 17:04:41 +0200

    wip: catch up sync

--- onYOURway/App/app.Debug.config.js
+++ onYOURway/App/app.Debug.config.js
@@ -1,5 +1,7 @@
 ﻿var config = {
+  appName: 'onYOURway.at (preview)',
   host: '', //use local api. Change to 'http://onyourway.at' to use production host
+  //host: 'http://onyourway.at',
   apiKey: {
     cloudmade: 'f8a57472669e4fcda99ebd5ed0f72918',
     lyrk: '804127fa700a46cfb039f49ac008a853'
