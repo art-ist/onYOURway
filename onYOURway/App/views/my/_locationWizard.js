d54929fd3881e0586c7fe2e0d976bb8432d13dba
art-ist <dev@art-ist.cc>
2014-05-31 16:57:36 +0200

    wip: getting in sync

--- onYOURway/App/views/my/_locationWizard.js
+++ onYOURway/App/views/my/_locationWizard.js
@@ -1,70 +1,90 @@
 ﻿define([
   'services/logger',
   'services/app',
+  'services/location',
   'services/auth'
-], function (logger, app, auth) {
+], function (logger, app, location, auth) {
 
-  var vm = function () {
-    var self = this;
+	var vm = function () {
+		var self = this;
 
-    self.app = app;
+		//#region lifecycle
 
-    self.activate = function (article, sub, parameters) {
-      if (article) {
-        self.currentArticle('#' + article);
-      }
+		self.activate = function (article, sub, parameters) {
+			if (article) {
+				self.currentArticle('#' + article);
+			}
 
-      logger.log('Wizard activated', '_locationWizard  - activate', app.location.loactionToEdit());
-      //app.location
-      //  .getLocation(138)
-      //  .then(function () {
-      //    logger.log('location assigned', '_locationWizard  - activate', app.location.loactionToEdit());
-      //  });
+			logger.log('Wizard activated', '_locationWizard  - activate', app.location.loactionToEdit());
+			//app.location
+			//  .getLocation(138)
+			//  .then(function () {
+			//    logger.log('location assigned', '_locationWizard  - activate', app.location.loactionToEdit());
+			//  });
 
-      return true;
-    };
+			return true;
+		};
 
-    self.attached = function (p1, p2, p3) {
-      //logger.log('view attached', 'profile', { 'p1': p1, 'p2': p2, 'p3': p3 });
-      app.navigateInPage(function navigated(href, target) {
-        self.currentArticle(href); //set vm property so databinding can use it
-      });
+		//self.attached = function (p1, p2, p3) { //Parameters
 
-      ////initialize slideshow
-      //  self.step = $("#neu")
-      //    .owlCarousel({          //initialize carousel (see: http://www.owlgraphic.com/owlcarousel/#customizing)
-      //      navigation: false, // Show next and prev buttons
-      //      slideSpeed: 500,
-      //      paginationSpeed: 400,
-      //      singleItem: true,
-      //      rewindNav: false
-      //    })
-      //  .data('owlCarousel')    //get instance of the api in self-idea
-      //    ;
-      //  $('.owl-pagination').appendTo($('#step_pagination'));
+		//	return true;
+		//};
 
-      return true;
-    };
+		//#endregion lifecycle
 
-    //// UI state
-    self.currentArticle = ko.observable('#uebersicht');
+		//// UI state
+		self.currentArticle = ko.observable('#uebersicht');
 
-    //// Data
-    self.location = app.location.loactionToEdit;
+		//// Properties
+		self.app = app;
+		self.location = location.selectedItem;
+		self.OpeningHours_Entries = ko.computed({
+			read: function () {
+				if (self.location().OpeningHours()) {
+					return self.location().OpeningHours().split(';');
+				}
+				else {
+					return[];
+				}
+			},
+			write: function (value) {
+				if (value[0])
+					return value.join('; ')
+			},
+			owner: self
+		});
 
-    self.addAlias = addAlias;
-    self.removeAlias = removeAlias;
+		//// Methods
+		self.addAlias = addAlias;
+		self.removeAlias = removeAlias;
 
-  };
-  return vm;
+	};
+	return vm;
 
-  function addAlias() {
-    var item = app.location.context.createEntity("LocationAlias:#onYOURway.Models");
-    app.location.loactionToEdit().Aliases.push(item);
-  }
-  function removeAlias(item) {
-    logger.log('Removing alias', '_locationWizard - removeAlias', item);
-    app.location.loactionToEdit().Aliases.remove(item);
-  }
+	//#region complex Properties
+
+	//#region Alias
+	function addAlias() {
+		var item = app.location.context.createEntity("LocationAlias:#onYOURway.Models");
+		vm.location().Aliases.push(item);
+	}
+	function removeAlias(item) {
+		logger.log('Removing alias', '_locationWizard - removeAlias', item);
+		vm.location().Aliases.remove(item);
+	}
+	//#endregion Alias
+
+	//#region OpeningHours
+
+	function addOpeningHours_Block() {
+		OpeningHours_Entries.push('');
+	}
+	function removeOpeningHours_Block(item) {
+		OpeningHours_Entries.remove(item);
+	}
+
+	//#endregion OpeningHours
+
+	//#endregion complex Properties
 
 }); //define
