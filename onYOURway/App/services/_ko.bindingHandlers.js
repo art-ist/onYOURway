2a661adc966055eae80c0c755d5dc341a98f3567
art-ist <dev@art-ist.cc>
2014-06-09 19:41:42 +0200

    add: enhance binding error messages with custom binding provider

--- onYOURway/App/services/_ko.bindingHandlers.js
+++ onYOURway/App/services/_ko.bindingHandlers.js
@@ -1,4 +1,29 @@
-﻿//#region input bindings
+﻿(function () {
+
+	//Catch binding exceptions using a custom binding provider
+	//see: http://www.knockmeout.net/2013/06/knockout-debugging-strategies-plugin.html
+	var existing = ko.bindingProvider.instance;
+
+	ko.bindingProvider.instance = {
+		nodeHasBindings: existing.nodeHasBindings,
+		getBindings: function (node, bindingContext) {
+			var bindings;
+			try {
+				bindings = existing.getBindings(node, bindingContext);
+			}
+			catch (ex) {
+				if (window.console && console.log) {
+					console.log("KO binding error: " + ex.message, node, bindingContext);
+				}
+			}
+			return bindings;
+		}
+	};
+
+})();
+
+
+//#region input bindings
 
 ////add ko binding for return key
 //ko.bindingHandlers.returnKey = {
