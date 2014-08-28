9344bb0b99735c12f1a2e6415af38cd09f0991be
art-ist <dev@art-ist.cc>
2014-08-28 10:32:59 +0200

    siteCollector: adresse aufgeteilt und bindings großteils angepasst

--- onYOURway/App/views/my/siteCollector.js
+++ onYOURway/App/views/my/siteCollector.js
@@ -10,8 +10,7 @@ define([
         self.app = app;
         self.location = location;
 
-    	site: ko.observable();
-
+		//lifecycle callbacks
         self.activate = function (queryString) {
             logger.log('activate', 'siteCollector', queryString);
             if (queryString && queryString.tag) {
@@ -26,13 +25,19 @@ define([
             location.siteCollectorMode(false);
         };
 
+		//breeze Entity for location/site/place
+        site = ko.observable();
+
+		//#region address
         self.street = ko.computed({
-            read: function () { return location.siteCollectorAddress && location.siteCollectorAddress() && location.siteCollectorAddress().address ? ((location.siteCollectorAddress().address.street ? location.siteCollectorAddress().address.street : '') + ' ' + (location.siteCollectorAddress().address.nr ? location.siteCollectorAddress().address.nr : '')) : '' },
+        	read: function () {
+        		return location.siteCollectorAddress && location.siteCollectorAddress() && location.siteCollectorAddress().address ?
+					location.siteCollectorAddress().address.street : '';
+        	},
             write: function (arg) {
                 var val = location.siteCollectorAddress();
                 if (val && val.address) {
                     val.address.street = arg;
-                    val.address.nr = '';
                 } else {
                     val = { address: { street: arg } };
                 }
@@ -40,14 +45,31 @@ define([
             },
             deferEvaluation: true
         });
+        self.nr = ko.computed({
+        	read: function () {
+        		return location.siteCollectorAddress && location.siteCollectorAddress() && location.siteCollectorAddress().address ?
+					location.siteCollectorAddress().address.nr : ''; //TODO: wird die (neue?) Verwendung von Hausnummern bei der Geolokation richtig umgesetzt?
+        	},
+        	write: function (arg) {
+        		var val = location.siteCollectorAddress();
+        		if (val && val.address) {
+        			val.address.nr = arg;
+        		} else {
+        			val = { address: { nr: arg } };
+        		}
+        		location.siteCollectorAddress(val);
+        	},
+        	deferEvaluation: true
+        });
         self.city = ko.computed({
-            read: function () { return location.siteCollectorAddress && location.siteCollectorAddress() && location.siteCollectorAddress().address ? ((location.siteCollectorAddress().address.postcode ? (location.siteCollectorAddress().address.postcode + ' ') : '') + (location.siteCollectorAddress().address.city ? location.siteCollectorAddress().address.city : '') + (location.siteCollectorAddress().address.country ? ((location.siteCollectorAddress().address.city ? ', ' : '') + location.siteCollectorAddress().address.country) : '')) : '' },
+        	read: function () {
+        		return location.siteCollectorAddress && location.siteCollectorAddress() && location.siteCollectorAddress().address ?
+					location.siteCollectorAddress().address.city : '';
+        	},
             write: function (arg) {
                 var val = location.siteCollectorAddress();
                 if (val && val.address) {
                     val.address.city = arg;
-                    val.address.postcode = '';
-                    val.address.country = '';
                 } else {
                     val = { address: { city: arg } };
                 }
@@ -55,9 +77,79 @@ define([
             },
             deferEvaluation: true
         });
+        self.postcode = ko.computed({
+        	read: function () {
+        		return location.siteCollectorAddress && location.siteCollectorAddress() && location.siteCollectorAddress().address ? 
+					location.siteCollectorAddress().address.postcode : '';
+        	},
+        	write: function (arg) {
+        		var val = location.siteCollectorAddress();
+        		if (val) {
+        			val.address.postcode = arg;
+        		} else {
+        			val = { address: { postcode: arg } };
+        		}
+        		location.siteCollectorAddress(val);
+        	},
+        	deferEvaluation: true
+        });
+        self.country = ko.computed({
+        	read: function () {
+        		return location.siteCollectorAddress && location.siteCollectorAddress() && location.siteCollectorAddress().address ?
+					location.siteCollectorAddress().address.country : '';
+        	},
+        	write: function (arg) {
+        		var val = location.siteCollectorAddress();
+        		if (val) {
+        			val.address.country = '';
+        		} else {
+        			val = { address: { country: arg } };
+        		}
+        		location.siteCollectorAddress(val);
+        	},
+        	deferEvaluation: true
+        });
+    	//#endregion address
+
+    	//#region position
+
+        self.lat = ko.computed({
+        	read: function () {
+        		var ll = location.siteCollectorMarker ? location.siteCollectorMarker().getLatLng() : null;
+        		return ll ? ll.lat : null;
+        	},
+        	write: function (arg) {
+        		var val = location.setSiteCollectorMarker();
+        		var newLatLng = new L.LatLng(arg, val.getLatLng().lng);
+        		val.setLatLng(newLatLng);
+        		//if (self.addressModeAuto) { //update address only if not entered manually (TODO)
+        		//	location.siteCollectorAddress(val);
+        		//}
+        	},
+        	deferEvaluation: true
+        });
+
+        self.lng = ko.computed({
+        	read: function () {
+        		var ll = location.siteCollectorMarker ? location.siteCollectorMarker().getLatLng() : null;
+        		return ll ? ll.lng : null;
+        	},
+        	write: function (arg) {
+        		var val = location.setSiteCollectorMarker();
+        		var newLatLng = new L.LatLng(val.getLatLng().lat, arg);
+        		val.setLatLng(newLatLng);
+        		//if (self.addressModeAuto) { //update address only if not entered manually (TODO)
+        		//	location.siteCollectorAddress(val);
+        		//}
+        	},
+        	deferEvaluation: true
+        });
+
+		//#endregion position
 
         self.setMarker = function () {
-            location.setSiteCollectorMarker(this.street() + ' ' + this.city());
+            //location.setSiteCollectorMarker(this.street() + ' ' + this.city());
+        	location.setSiteCollectorMarker(location.siteCollectorAddress());
         }
 
     };
