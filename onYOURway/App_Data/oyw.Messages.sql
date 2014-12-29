7379953d3f6f451e4599cf1a1ab0843e3c9bf54f
art-ist <dev@art-ist.cc>
2014-12-29 16:30:15 +0100

    add: App_Data\oyw.Messages.sql

---
+++ onYOURway/App_Data/oyw.Messages.sql
@@ -0,0 +1,24 @@
+Truncate Table oyw.[Message];
+
+INSERT INTO oyw.[Message] (Locale, [Key], [Text]) VALUES
+
+	 ('', 'home.preview', 'Public Preview with Testdata')
+	,('de', 'home.preview', 'Public Preview mit Testdaten')
+
+	,('', 'nav.search.advanced.text', 'When & Where')
+	,('de', 'nav.search.advanced.text', 'Wann & Wo')
+	,('', 'nav.search.advanced.title', 'Advanced Search')
+	,('de', 'nav.search.advanced.title', 'Erweiterte Suche')
+	,('', 'nav.search.placeholder', 'Fruit, Bakery, Fair, ...')
+	,('de', 'nav.search.placeholder', 'Obst, Heurige, Bio, ...')
+	,('', 'nav.search.title', 'Search')
+	,('de', 'nav.search.title', 'Suchen')
+
+	,('', 'siteCollector.selectRegion', 'Selected region/map')
+	,('de', 'siteCollector.selectRegion', 'Karte/Region wählen')
+	,('', 'siteCollector.title.add', 'Add a new site')
+	,('de', 'siteCollector.title.add', 'Standort hinzufügen')
+
+;
+
+Select * From oyw.[Message]
