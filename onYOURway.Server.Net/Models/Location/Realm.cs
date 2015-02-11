e11f7daae0c915a8e67552d0b4e8dbfb8aaac2e7
art-ist <dev@art-ist.cc>
2015-02-11 14:46:48 +0100

    wip: datamodel - extensions for realm

--- onYOURway.Server.Net/Models/Location/Realm.cs
+++ onYOURway.Server.Net/Models/Location/Realm.cs
@@ -14,22 +14,34 @@ namespace onYOURway.Models {
 		/// Unique id
 		/// </summary>
 		[Key]
-		public Int32 Id { get; set; }
-
-		/// <summary>
-		/// Pattern to recognize Realm to Url
-		/// </summary>
-		public String UrlPattern { get; set; }
-
+		public String Key { get; set; }
+		
 		/// <summary>
 		/// Native name e.g. used as title in realm description
 		/// </summary>
 		public String Name { get; set; }
 
 		/// <summary>
-		/// Native description
+		/// RegEx pattern to recognize Realm from url
+		/// </summary>
+		public String UrlPattern { get; set; }
+
+		/// <summary>
+		/// Comma seperated list of items to return in search suggestions
+		/// </summary>
+		/// <example>Location</example>
+		/// <example>location,tag,city,street</example>
+		public String SuggestItems { get; set; }
+
+		/// <summary>
+		/// Description in the native language of the realm. In case of international activity we suggest using english here.
 		/// </summary>
 		public String Description { get; set; }
+
+		/// <summary>
+		/// Path to the logo for the Realm (should be relative to the path /Content/{RealmKey} and be 160x160 pixel)
+		/// </summary>
+		public String LogoUrl { get; set; }
 
 		/// <summary>
 		/// Root tag of the taxonomy used by this realm thus selecting the available taxonomy
@@ -62,7 +74,7 @@ namespace onYOURway.Models {
 		/// Key of realm to be localized
 		/// </summary>
 		[Key, Column(Order = 0)]
-		public Int32 RealmId { get; set; }
+		public Int32 RealmKey { get; set; }
 
 		/// <summary>
 		/// Locale e.g. "de-DE" or language e.g. "de" of localization
