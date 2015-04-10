a3cfd832de086dca621979fc0c57c2a8f5edc71f
art-ist <dev@art-ist.cc>
2015-04-10 12:28:26 +0200

    add: clientModelExtensions for dynamic translation of selected categories in siteCollector (wip)

--- onYOURway.Client.WebApp/App/models/clientModelExtensions.js
+++ onYOURway.Client.WebApp/App/models/clientModelExtensions.js
@@ -15,76 +15,75 @@
 		// and: http://www.breezejs.com/documentation/extending-entities
 		var metadata = context.metadataStore;
 
-		metadata.registerEntityTypeCtor("Location", Location); // "Location:#onYOURway.Models"
+		metadata.registerEntityTypeCtor("Category", Category); // "Location:#onYOURway.Models"
 		metadata.registerEntityTypeCtor("EntryCategory", EntryCategory, EntryCategoryInitializer); // "EntryCategory:#onYOURway.Models"
 
 	} //_extendEntities
 
 	//#region Location
 
-	var Location = function () {
-		this.kind = "";
-		this.tags = [];
-		this.open = [];
-	};
+	//var Location = function () {
+	//	this.kind = "";
+	//	this.tags = [];
+	//	this.open = [];
+	//};
 
 	//#endregion Location
 
-	////#region Category
+	//#region Category
 
-	//function Category() {
-	//	//extensions in the constructor will be turned into observables by breeze and on export be serialized as unmapped
+	function Category() {
+		//extensions in the constructor will be turned into observables by breeze and on export be serialized as unmapped
+	} //Category
+	var CategoryInitializer = function (self) {
+		//extensions in the post-construction initializer will be NOT made observable by breeze will NOT be exported
 
-	//} //Category
+		var local = ko.computed({
+			read: function () {
+				require(['services/app'], function (app) {
+					//logger.log('Local', 'mind - Node', { node: self, texts: self.Texts(), lang: app.lang() });
 
-	//var CategoryInitializer = function (self) {
-	//	//extensions in the post-construction initializer will be NOT made observable by breeze will NOT be exported
+					//nothing there
+					if (!self.Category || !self.Category() || !self.Category().Names) { return null; }
 
-	//	var local = ko.computed({
-	//		read: function () {
-	//			require(['services/app'], function (app) {
-	//				//logger.log('Local', 'mind - Node', { node: self, texts: self.Texts(), lang: app.lang() });
+					var names = self.Category().Names();
+					//empty array 
+					if (!names.length) { return null; }
+					//find and return first localized text
+					var i = 0;
+					for (i = 0; i < names.length; i++) {
+						if (names[i].Lang() === app.lang()) {
+							return names[i];
+						}
+					}
+					//return first neutral Names
+					for (i = 0; i < self.Names().length; i++) {
+						if (!names[i].Lang || !self.Names()[i].Lang()) {
+							return names[i];
+						}
+					}
+					//return whatever you have
+					return snames[0];
+				});
+			}
+			, owner: self
+			//,deferEvaluation: true //required because Entity properties are not yet defined
+		}, self); //.extend({ notify: 'always' });
 
-	//				//nothing there
-	//				if (!self.Category || !self.Category() || !self.Category().Names) { return null; }
+		//self.Local = local;
+		self.LName = ko.computed(function () { var l = local(); if (l) return l.Name(); }, self);
+		self.LDescription = ko.computed(function () { var l = local(); if (l) return l.Description(); }, self);
 
-	//				var names = self.Category().Names();
-	//				//empty array 
-	//				if (!names.length) { return null; }
-	//				//find and return first localized text
-	//				var i = 0;
-	//				for (i = 0; i < names.length; i++) {
-	//					if (names[i].Lang() === app.lang()) {
-	//						return names[i];
-	//					}
-	//				}
-	//				//return first neutral Names
-	//				for (i = 0; i < self.Names().length; i++) {
-	//					if (!names[i].Lang || !self.Names()[i].Lang()) {
-	//						return names[i];
-	//					}
-	//				}
-	//				//return whatever you have
-	//				return snames[0];
-	//			});
-	//		}
-	//		, owner: self
-	//		//,deferEvaluation: true //required because Entity properties are not yet defined
-	//	}, self); //.extend({ notify: 'always' });
+	};
 
-	//	//self.Local = local;
-	//	self.LName = ko.computed(function () { var l = local(); if (l) return l.Name(); }, self);
-	//	self.LDescription = ko.computed(function () { var l = local(); if (l) return l.Description(); }, self);
-
-	//};
-
-	////#endregion Category
+	//#endregion Category
 
 	//#region EntryCategory
 
 	function EntryCategory() {
 		//extensions in the constructor will be turned into observables by breeze and on export be serialized as unmapped
-
+		this.LName = '';
+		this.LDescription = '';
 	} //EntryCategory
 
 	var EntryCategoryInitializer = function (self) {
